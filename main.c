808ab5efbcad6e075615ad841b0bb344d491a948
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2021-11-03 23:15:56 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int count = 0;
+	while(count <= 255)
+    {
+         printf("ASCII value of %c is %d\n", count, count);
+        count++;
+
+    }
+
+
+	return 0;
+}
